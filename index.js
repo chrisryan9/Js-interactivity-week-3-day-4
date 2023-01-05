bd3f0c7dea6b1f1b5b4806cfa8e408cc7fcbaf72
Chris Ryan <chrisryanassociates@gmail.com>
2023-01-05 15:57:08 -0700

    first

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log('hello world');
+
+
+
+const addMovie = (event) => {
+    event.preventDefault();
+    let inputField = document.querySelector('input');
+    
+    const movie = document.createElement('li');
+    
+    const movieTitle = document.createElement("span");
+    movieTitle.addEventListener('click', crossOffMovie);
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.addEventListener('click', deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    const list = document.querySelector('ul');
+    list.appendChild(movie);
+    
+    inputField.value = "";
+};
+
+
+document.querySelector("form").addEventListener("submit", addMovie);
+const message = document.querySelector('#message');
+
+const deleteMovie = (event) => {
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`;
+    
+    revealMessage();
+
+    event.target.parentNode.remove();
+};
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle('checked');
+
+    if(event.target.classList.contains('checked') === true) {
+        message.textContent = `${event.target.textContent} watched!`;
+    } else {
+        message.textContent = `${event.target.textContent} added back!`;
+    }
+
+    revealMessage();
+};
+
+
+const revealMessage = () => {
+
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 2000);
+}
